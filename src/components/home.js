6051469f94b31bae323a53e9fd617d5d80bee9cf
Vofy <tomas.batelka@email.cz>
2022-01-19 00:34:45 +0100

    Upravený manifest

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,11 @@
 export default function Home() {
     return (
         <div style={{display: "grid", justifyItems: "center", minHeight: "calc(100vh - 63px", alignContent: "center"}}>
-            <h1 class="text-uppercase" style={{textAlign: "center"}}>
-                <span class="backdrop-item">Vítejte</span>
-                <span class="backdrop-item">na</span>
-                <span class="backdrop-item">perFEKTním</span>
-                <span class="backdrop-item">drillu</span>
+            <h1 className="text-uppercase" style={{textAlign: "center"}}>
+                <span className="backdrop-item">Vítejte</span>
+                <span className="backdrop-item">na</span>
+                <span className="backdrop-item">perFEKTním</span>
+                <span className="backdrop-item">drillu</span>
             </h1>
             <img src="/images/meme.jpg" height="500" />
         </div>
