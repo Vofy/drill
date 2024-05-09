2ec33ae4e203d863409965d004dd2528490c0ad2
Tomáš Batelka <tomas.batelka@email.cz>
2024-05-09 21:53:01 +0200

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -21,9 +21,7 @@ export default function Home(props) {
                 <span className="backdrop-item">perFEKTním</span>
                 <span className="backdrop-item">drillu</span>
             </h1>
-            <a href="/xpc/elb/kt"><button>XPC-ELB/KT shortcut</button></a>
-            <a href="/xpc/elb/zt"><button>XPC-ELB/ZT shortcut</button></a>
-            <a href="/bpc/psd/lc/kt"><button>BPC-PSD/LC/KT shortcut</button></a>
+            <a href="/xpc/psd/zkouska"><button>BPC-PSD/Zkouška shortcut</button></a>
             <img alt={randomMeme.name} src={randomMeme.path} style={{maxHeight: "70vh", maxWidth: "100%"}} />
         </div>
     )
